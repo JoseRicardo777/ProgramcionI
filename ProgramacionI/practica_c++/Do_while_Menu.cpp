c027b6476d6e71fce4fa2d01d945056aec33d07a
JoseRicardo777 <155786768+JoseRicardo777@users.noreply.github.com>
2024-01-15 17:35:01 -0800

    Update Do_while_Menu.cpp

--- ProgramacionI/practica_c++/Do_while_Menu.cpp
+++ ProgramacionI/practica_c++/Do_while_Menu.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main() {
-	int opcion; // Variable para almacenar la opción seleccionada por el usuario
-	opcion = 1; // Inicializa la opción en 1
+	int opcion; // Variable para almacenar la opciÃ³n seleccionada por el usuario
+	//opcion = 1; // Inicializa la opciÃ³n en 1
 	
-	// Inicia un bucle do-while que se ejecuta mientras la opción no sea 0
+	// Inicia un bucle do-while que se ejecuta mientras la opciÃ³n no sea 0
 	do {
-		// Imprime el menú principal
+		// Imprime el menÃº principal
 		cout << "MENU PRINCIPAL" << endl;
 		cout << "1. OPCION UNO" << endl;
 		cout << "2. OPCION DOS" << endl;
 		cout << "0. SALIR" << endl;
 		
-		// Lee la opción ingresada por el usuario
+		// Lee la opciÃ³n ingresada por el usuario
 		cin >> opcion;
 		
-		// Utiliza un switch para realizar acciones según la opción seleccionada
+		// Utiliza un switch para realizar acciones segÃºn la opciÃ³n seleccionada
 		switch (opcion) {
 		case 1:
 			cout << "Eligio la opcion 1" << endl;
@@ -30,7 +30,7 @@ int main() {
 		default:
 			cout << "No escogio una opcion correcta " << endl;
 		}
-	} while (opcion != 0); // El bucle se repite mientras la opción no sea 0
+	} while (opcion != 0); // El bucle se repite mientras la opciÃ³n no sea 0
 	
 	return 0; // Fin del programa
 }
