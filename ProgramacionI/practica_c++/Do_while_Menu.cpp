534b74c9118bc7029c9b51f821375410f1ff2423
JoseRicardo777 <155786768+JoseRicardo777@users.noreply.github.com>
2024-01-15 17:33:06 -0800

    Add files via upload

---
+++ ProgramacionI/practica_c++/Do_while_Menu.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int opcion; // Variable para almacenar la opción seleccionada por el usuario
+	opcion = 1; // Inicializa la opción en 1
+	
+	// Inicia un bucle do-while que se ejecuta mientras la opción no sea 0
+	do {
+		// Imprime el menú principal
+		cout << "MENU PRINCIPAL" << endl;
+		cout << "1. OPCION UNO" << endl;
+		cout << "2. OPCION DOS" << endl;
+		cout << "0. SALIR" << endl;
+		
+		// Lee la opción ingresada por el usuario
+		cin >> opcion;
+		
+		// Utiliza un switch para realizar acciones según la opción seleccionada
+		switch (opcion) {
+		case 1:
+			cout << "Eligio la opcion 1" << endl;
+			break;
+		case 2:
+			cout << "Eligio la opcion 2" << endl;
+			break;
+		case 0:
+			cout << "Eligio la opcion 0" << endl;
+			break;
+		default:
+			cout << "No escogio una opcion correcta " << endl;
+		}
+	} while (opcion != 0); // El bucle se repite mientras la opción no sea 0
+	
+	return 0; // Fin del programa
+}
+
---
+++ ProgramacionI/practica_c++/mayuscula_a_minuscula_vocal.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype>  // Necesario para tolower
+using namespace std;
+
+int main() {
+	char caracter;
+	cout << "Coloque un caracter: ";
+	cin >> caracter;
+	
+	// Convertir el caracter a minúscula
+	char caracter_minuscula = tolower(caracter); // LA FUNCION PARA CONVERTIR MAYUSCULA A MIN
+	
+	switch (caracter_minuscula) {
+	case 'a':
+	case 'e':
+	case 'i':
+	case 'o':
+	case 'u':
+		cout << "Es vocal";
+		break;
+	default:
+		cout << "No es vocal";
+	}
+	
+	return 0;
+}
+
